69ec668533d8e5b72559884462994e838b4f0a4e
Krzysztof Fila <kfila17@gmail.com>
2021-02-08 20:37:43 +0000

    Kamien, papier nozyce za pomoca petli if

---
+++ main.py
@@ -0,0 +1,28 @@
+print("Zagrajmy w kamień, papier, nożyce :D")
+user_1 = input('User1 :')
+user_2 = input('User2 :')
+
+if user_1=='papier':
+    if user_2 =='kamien':
+        print("wygrywa User1")
+    if user_2 =='nozyce':
+        print("wygrywa User2")
+    if user_2 =='papier'
+        print("remis")
+
+if user_1 =='kamien':
+    if user_2 =='kamien':
+        print("remis")
+    if user_2 =='nozyce':
+        print("wygrywa User1")
+    if user_2 =='papier'
+        print("wygrywa User2")
+
+if user_1 =='nozyce':
+    if user_2 =='nozyce':
+        print("remis")
+    if user_2 =='papier':
+        print("wygrywa User1")
+    if user_2 =='kamien'
+        print("wygrywa User2")
+

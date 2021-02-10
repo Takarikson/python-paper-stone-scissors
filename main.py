551a0181f89f16782e5b3d1801423cdb08b0e497
Krzysztof Fila <kfila17@gmail.com>
2021-02-10 12:27:14 +0000

    add else

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ user_2 = input('User2 :')
 if user_1=='papier':
     if user_2 =='kamien':
         print("wygrywa User1")
-    if user_2 =='nozyce':
+    else:
         print("wygrywa User2")
 
 if user_1 =='kamien':
     if user_2 =='nozyce':
         print("wygrywa User1")
-    if user_2 =='papier':
+    else:
         print("wygrywa User2")
 
 if user_1 =='nozyce':
     if user_2 =='papier':
         print("wygrywa User1")
-    if user_2 =='kamien':
+    else:
         print("wygrywa User2")
-
+        
 if user_1 == user_2:
     print("remis")
 
